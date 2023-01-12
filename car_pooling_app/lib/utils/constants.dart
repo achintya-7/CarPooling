af22bb3dccc0e6ca61075f8dc3949ba66a79a0c3
Achintya <achintya22052000@gmail.com>
2023-01-12 11:43:54 +0530

    added an easy toggle between dev and local server

--- car_pooling_app/lib/utils/constants.dart
+++ car_pooling_app/lib/utils/constants.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import "package:latlong2/latlong.dart";
 
-// const String apiUrlLocal = "http://10.0.2.2:8080/";
-const String apiUrlLocal = "https://car-pooling.fly.dev/";
+const bool local = false;
+
+const String apiUrlLocal = local ? "http://10.0.2.2:8080/" : "https://car-pooling.fly.dev/";
 
 LatLng newDelhi = LatLng(28.61992743538245, 77.20905101733563);
 
