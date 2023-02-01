8f847a16aa90c92686fb1f1a1bef6b66639fc1c6
Achintya <achintya22052000@gmail.com>
2023-02-01 15:02:32 +0530

    added swipe action widget

--- car_pooling_app/lib/main.dart
+++ car_pooling_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:car_pooling_app/firebase_options.dart';
-import 'package:car_pooling_app/service/auth_service.dart';
 import 'package:car_pooling_app/routes.dart';
+import 'package:car_pooling_app/service/auth_service.dart';
 import 'package:car_pooling_app/utils/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
--- car_pooling_app/lib/widgets/upcoming_ride_widgets.dart
+++ car_pooling_app/lib/widgets/upcoming_ride_widgets.dart
@@ -1,6 +1,8 @@
 import 'package:car_pooling_app/model/rides/rides_model.dart';
+import 'package:car_pooling_app/service/auth_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:slide_action/slide_action.dart';
 
 // class GetCurrentRideFutureBuilder extends StatelessWidget {
 //   const GetCurrentRideFutureBuilder({
@@ -40,7 +42,8 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class RideInfoWidget extends StatelessWidget {
   const RideInfoWidget({
-    Key? key, required this.ride,
+    Key? key,
+    required this.ride,
   }) : super(key: key);
 
   final Ride ride;
@@ -49,68 +52,141 @@ class RideInfoWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Theme(
       data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-      child: ExpansionTile(
-        textColor: Colors.black,
-        collapsedTextColor: Colors.black,
-        title: Text(ride.timestamp.toString()),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 4),
-            RichText(
-              maxLines: 1,
-              text: TextSpan(
-                style: const TextStyle(color: Colors.black, fontSize: 16),
-                children: [
-                  const WidgetSpan(
-                      child: Icon(
-                    Icons.place,
-                    size: 19,
-                    color: Colors.red,
-                  )),
-                  TextSpan(
-                    text: ride.origin,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 4),
-            RichText(
-              maxLines: 1,
-              text: TextSpan(
-                style: const TextStyle(color: Colors.black, fontSize: 16),
-                children: [
-                  const WidgetSpan(
-                      child: Icon(
-                    Icons.place,
-                    size: 19,
-                    color: Colors.green,
-                  )),
-                  TextSpan(
-                    text: ride.destination,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 4),
-          ],
-        ),
-        children: ride.passengers
-            .map(
-              (e) => ListTile(
-                title: Text(e.name),
-                subtitle: Text(
-                  e.origin,
-                  maxLines: 2,
-                  style: const TextStyle(color: Colors.black54),
+      child: ListTileTheme(
+        contentPadding: const EdgeInsets.all(0),
+        minLeadingWidth: 0,
+        child: ExpansionTile(
+          textColor: Colors.black,
+          collapsedTextColor: Colors.black,
+          title: Text(ride.timestamp.toString()),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 4),
+              RichText(
+                maxLines: 1,
+                text: TextSpan(
+                  style: const TextStyle(color: Colors.black, fontSize: 16),
+                  children: [
+                    const WidgetSpan(
+                        child: Icon(
+                      Icons.place,
+                      size: 19,
+                      color: Colors.red,
+                    )),
+                    TextSpan(
+                      text: ride.origin,
+                    ),
+                  ],
                 ),
-                trailing: ride.email == e.email
-                    ? const Icon(Icons.local_taxi)
-                    : const Icon(Icons.person),
               ),
-            )
-            .toList(),
+              const SizedBox(height: 4),
+              RichText(
+                maxLines: 1,
+                text: TextSpan(
+                  style: const TextStyle(color: Colors.black, fontSize: 16),
+                  children: [
+                    const WidgetSpan(
+                        child: Icon(
+                      Icons.place,
+                      size: 19,
+                      color: Colors.green,
+                    )),
+                    TextSpan(
+                      text: ride.destination,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 6),
+              if (ride.started)
+                const Text(
+                  'Ride Started...',
+                  style: TextStyle(color: Colors.green, fontSize: 24),
+                )
+              else if (ride.email == AuthService().getCurrentUser()?.email)
+                const SizedBox(height: 5),
+                const SlideToAcceptWidget()
+            ],
+          ),
+          children: ride.passengers
+              .map(
+                (e) => ListTile(
+                  title: Text(e.name),
+                  subtitle: Text(
+                    e.origin,
+                    maxLines: 2,
+                    style: const TextStyle(color: Colors.black54),
+                  ),
+                  trailing: ride.email == e.email
+                      ? const Icon(Icons.local_taxi)
+                      : const Icon(Icons.person),
+                ),
+              )
+              .toList(),
+        ),
       ),
+    );
+  }
+}
+
+class SlideToAcceptWidget extends StatelessWidget {
+  const SlideToAcceptWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideAction(
+      trackHeight: 52,
+      trackBuilder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 8,
+              ),
+            ],
+          ),
+          child: const Center(
+            child: Text(
+              "Swipe to Start Ride",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+        );
+      },
+      thumbBuilder: (context, state) {
+        return Container(
+          margin: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Center(
+            child: state.isPerformingAction
+                ? const CupertinoActivityIndicator(
+                    color: Colors.white,
+                  )
+                : const Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                  ),
+          ),
+        );
+      },
+      action: () async {
+        // Async operation
+        await Future.delayed(
+          const Duration(seconds: 2),
+          () => debugPrint("Hello World"),
+        );
+      },
     );
   }
 }
